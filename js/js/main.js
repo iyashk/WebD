a1f081f618c1892c43f98408d28cd8e101bbfdf8
yashwanth kiran <yashwanthkiran2624@gmail.com>
2022-11-12 10:09:43 +0530

    lecture 9

--- js/js/main.js
+++ js/js/main.js
@@ -9,3 +9,17 @@ const number = 67;
 const string = "67";
 console.log(number==string);
 console.log(Number.parseFloat("1234.4556789gh").toFixed(2));
+
+
+console.log(Math.trunc(Math.E),Math.PI);
+
+let maths = 59;
+if (maths>60){
+    console.log("NOt a rahul");
+}
+else if (maths===59){
+    console.log("is Rahul");
+}
+else{
+    console.log("some other");
+}
