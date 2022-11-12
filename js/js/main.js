3a2d6fe3e17c2fc6e4f5186685bc4e6ee61a84fd
yashwanth kiran <yashwanthkiran2624@gmail.com>
2022-11-12 09:43:42 +0530

    lecture 4

---
+++ js/js/main.js
@@ -0,0 +1,11 @@
+console.log("Hello World!");
+console.log(typeof "Hero");
+
+const myVar = "Maths";
+console.log(myVar.length);
+console.log(myVar);
+
+const number = 67;
+const string = "67";
+console.log(number==string);
+console.log(Number.parseFloat("1234.4556789gh").toFixed(2));
