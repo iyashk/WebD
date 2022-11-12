6c11cee7893330333b1bb31b0478470c6ff758e5
yashwanth kiran <yashwanthkiran2624@gmail.com>
2022-11-12 10:47:43 +0530

    prompt, alert and confirm

--- js/js/main.js
+++ js/js/main.js
@@ -23,3 +23,25 @@ else if (maths===59){
 else{
     console.log("some other");
 }
+
+
+// user input
+
+let is_true = confirm("accept for permissoin");
+if (is_true){
+    console.log("accepted");
+    let name = prompt("Enter your name in the field");
+    let name_bool = name ?? "default name loaded";
+    if (name_bool === name){
+        console.log(name.length>0?name:"name not entered")
+        
+    }
+    else{
+        console.log("name not entered")
+    }
+    // let name_bool = name.length>0?console.log(name ?? "default name loaded");
+    console.log();
+}
+else{
+    console.log("cancelled");
+}
